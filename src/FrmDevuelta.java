75959ed5f2dbe6467956bec2f0fbe41865c01703
Juan Camilo Botero <jcbotero.ing@gmail.com>
2026-03-21 21:14:50 -0500

    Se agregan los eventos al aplicativo

--- src/FrmDevuelta.java
+++ src/FrmDevuelta.java
@@ -2,6 +2,7 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
@@ -9,6 +10,12 @@ import javax.swing.table.DefaultTableModel;
 
 public class FrmDevuelta extends JFrame {
 
+    JComboBox cmbDenominacion;
+    int[] denominaciones = { 100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50 };
+    int[] existencias=new int[denominaciones.length];
+    JTextField txtExistencia;
+
+    
     // metodo constructor
 
     public FrmDevuelta(){
@@ -21,11 +28,10 @@ public class FrmDevuelta extends JFrame {
         lblDenominacion.setBounds(10, 10, 100, 25);
         add(lblDenominacion);
 
-        JComboBox cmbDenominacion = new JComboBox();
+        cmbDenominacion = new JComboBox();
         cmbDenominacion.setBounds(220, 10, 100, 25 );
         add(cmbDenominacion);
 
-        int[] denominaciones = { 100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50 };
         for (int denominacion : denominaciones) {
             cmbDenominacion.addItem(denominacion);
         }
@@ -34,7 +40,7 @@ public class FrmDevuelta extends JFrame {
         btnExistencia.setBounds(10, 40, 200, 25);
         add(btnExistencia);
 
-        JTextField txtExistencia = new JTextField();
+        txtExistencia = new JTextField();
         txtExistencia.setBounds( 220, 40, 50, 25);
         add(txtExistencia);
 
@@ -61,9 +67,39 @@ public class FrmDevuelta extends JFrame {
         DefaultTableModel dtm=new DefaultTableModel(null, encabezados);
         tblDevuelta.setModel(dtm);
 
+        // eventos
+        btnExistencia.addActionListener(e ->{
+            actualizarExistencia();
+        });
+
+        btnDevuelta.addActionListener(e ->{
+            calcularDevuelta();
+        });
+
+        cmbDenominacion.addActionListener(e ->{
+            consultarExistencia();
+        });
 
 
+    }
 
+    private void consultarExistencia() {
+        if(cmbDenominacion.getSelectedIndex() >=0) {
+            //JOptionPane.showMessageDialog(null, denominaciones[cmbDenominacion.getSelectedIndex()]);
+            txtExistencia.setText(String.valueOf(existencias[cmbDenominacion.getSelectedIndex()]));
+        }
+    }
+
+    private void actualizarExistencia() {
+        //JOptionPane.showMessageDialog(null,"Hizo clic en actualizar Existencia");
+        if(cmbDenominacion.getSelectedIndex() >=0) {
+            existencias[cmbDenominacion.getSelectedIndex()] = Integer.parseInt(txtExistencia.getText());
+        }
+
+    }
+
+    private void calcularDevuelta() {
+        JOptionPane.showMessageDialog(null,"Hizo clic en calcular devuelta");
     }
 
 }
